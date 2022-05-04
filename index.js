601ab1a3e3d70d7064b72ca78a83a2f5c2603a32
Anton Reshetov <reshetov.art@gmail.com>
2022-05-04 07:59:24 +0300

    fix: exclude deleted snippets from fetch

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ import alfy from 'alfy'
 
 const data = await alfy.fetch('http://localhost:3033/snippets/embed-folder')
 
-const items = alfy.inputMatches(data, 'name').map((element) => ({
-  title: element.name,
-  subtitle: element.folder?.name || 'Inbox',
-  arg: element.content[0].value
-}))
+const items = alfy
+  .inputMatches(data, 'name')
+  .filter((i) => !i.isDeleted)
+  .map((element) => ({
+    title: element.name,
+    subtitle: element.folder?.name || 'Inbox',
+    arg: element.content[0].value,
+  }))
 
 alfy.output(items)
